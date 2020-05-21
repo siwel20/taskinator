c2904098ad27e9097e2d4ee33b4f03a0afca9cf9
siwel20 <plthrock@gmail.com>
2020-05-20 20:14:48 -0600

    added edit function to task into form

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,13 +99,39 @@ var deleteTask = function(taskId) {
 
 };
 
+var editTask = function(taskId) {
+    
+
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    // get content from teh taskname and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+    
+};
+
+
 buttonEl.addEventListener("submit", taskFormHandler);
 
 var taskButtonHandler = function(event) {
-
-    if (event.target.matches(".delete-btn")) {
+    // get target element from event
+    var targetEl = event.target;
+    // edit button was clicked
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    // delete button was clicked
+    else if (targetEl.matches(".delete-btn")) {
         // get the element's task id
-        var taskId = event.target.getAttribute("data-task-id");
+        var taskId = targetEl.getAttribute("data-task-id");
         deleteTask(taskId);
     }
 };
